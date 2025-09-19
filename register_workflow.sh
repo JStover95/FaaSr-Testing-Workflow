51c2134bcbff0f50c1f2e1bd72786302962ba40e
Jesse Stover <j.stover.r@gmail.com>
2025-09-19 14:25:01 -0500

    Added register workflow helper script

---
+++ register_workflow.sh
@@ -0,0 +1,16 @@
+WORKFLOW_FILE="main.json"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --workflow-file)
+            WORKFLOW_FILE=$2
+            shift 2
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+    esac
+done
+
+export $(cat .env | xargs)
+python scripts/register_workflow.py --workflow-file $WORKFLOW_FILE
